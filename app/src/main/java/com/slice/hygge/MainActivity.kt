bec6ab3790b5f506d4dd8234327f655f90c770ca
Saismita09 <saismita.panda@yahoo.com>
2020-06-06 16:03:06 +0530

    Initial commit

---
+++ app/src/main/java/com/slice/hygge/MainActivity.kt
@@ -0,0 +1,209 @@
+package com.slice.hygge
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.health.TimerStat
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.slice.hygge.util.PrefUtil
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds: Long = 0
+    private var timerState = TimerState.Stopped
+
+    private var secondsRemaining = 0L
+
+    private lateinit var mp: MediaPlayer
+    private var totalTime: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_spa)
+        supportActionBar?.title = "       Hygge - Be Present "
+        mp = MediaPlayer.create(this, R.raw.song)
+        mp.isLooping = true
+        mp.setVolume(100.0f, 100.0f)
+        totalTime = timerLengthSeconds
+
+
+        fab_start.setOnClickListener { v ->
+            startTimer()
+            mp.start()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+
+        fab_pause.setOnClickListener { v ->
+            timer.cancel()
+            mp.pause()
+            timerState = TimerState.Paused
+            updateButtons()
+
+        }
+        fab_stop.setOnClickListener { v ->
+            timer.cancel()
+            mp.pause()
+            onTimerFinished()
+
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        initTimer()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running) {
+            timer.cancel()
+            mp.pause()
+        } else if (timerState == TimerState.Paused) {
+
+
+        }
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+
+    }
+
+
+    private fun initTimer() {
+        timerState = PrefUtil.getTimerState(this)
+
+        //we don't want to change the length of the timer which is already running
+        //if the length was changed in settings while it was backgrounded
+        if (timerState == TimerState.Stopped)
+            setNewTimerLength()
+        else
+            setPreviousTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+            PrefUtil.getSecondsRemaining(this)
+        else
+            timerLengthSeconds
+
+        //resume where we left off
+        if (timerState == TimerState.Running) {
+            startTimer()
+
+            mp.start()
+        }
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun onTimerFinished() {
+        timerState = TimerState.Stopped
+        mp.pause()
+
+        //set the length of the timer to be the one set in SettingsActivity
+        //if the length was changed when the timer was running
+        setNewTimerLength()
+
+        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun startTimer() {
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+
+    }
+
+
+    private fun setNewTimerLength() {
+        val lengthInMinutes = PrefUtil.getTimerLength(this)
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(this)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI() {
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        textView_countdown.text =
+            "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+    private fun updateButtons() {
+        when (timerState) {
+            TimerState.Running -> {
+                fab_start.isEnabled = false
+                fab_pause.isEnabled = true
+                fab_stop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = false
+            }
+            TimerState.Paused -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = true
+            }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/slice/hygge/util/PrefUtil.kt
@@ -0,0 +1,67 @@
+package com.slice.hygge.util
+
+import android.content.Context
+import android.os.health.TimerStat
+import android.preference.PreferenceManager
+import com.slice.hygge.MainActivity
+import java.security.AccessControlContext
+import java.util.prefs.Preferences
+
+class PrefUtil {
+    companion object {
+        //variables inside this act as static members
+        private const val TIMER_LENGTH_ID = "com.slice.hygge.timer_length"
+
+        fun getTimerLength(context: Context): Int {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getInt(TIMER_LENGTH_ID, 10)
+        }
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.slice.timer.prev_length"
+
+        fun getPreviousTimerLengthSeconds(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
+        }
+
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "com.slice.timer.timer_state"
+
+        fun getTimerState(context: Context): MainActivity.TimerState {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.values()[ordinal]
+
+
+        }
+
+        fun setTimerState(state: MainActivity.TimerState, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+
+        }
+
+        private const val SECONDS_REMAINING = "com.slice.timer.prev_length"
+
+        fun getSecondsRemaining(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING, 0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING, seconds)
+            editor.apply()
+        }
+
+
+    }
+
+}
